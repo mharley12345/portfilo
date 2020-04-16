00355cd6a0967c25d2fe18cf0d3ec7b53f4d2263
Mike Harley <mharley12345@gmail.com>
2020-04-15 19:50:13 -0700

    .

--- src/index.js
+++ src/index.js
@@ -10,11 +10,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import "./styles/index.scss";
 
 ReactDOM.render(
-	<div className="my-new">
+	
+
+	<div id="overFlow" className="my-new">
 		<Navbar />
 		<Footer />
 	</div>, document.getElementById('root'));
-
+	
+    
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
