fcd141a6d20724a53cef7de980c31376fa489568
mharley12345 <info@mike-harley.tech>
2020-07-08 21:13:22 -0700

    .

---
+++ build/precache-manifest.24ca4e0bacbde323df264c4419701de8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf674e2035ad23c5a7f08fde343b5de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc1181877fe7195304ab",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "169e0e2dacae839997b5",
+    "url": "/static/css/main.5474a78e.chunk.css"
+  },
+  {
+    "revision": "cc1181877fe7195304ab",
+    "url": "/static/js/2.f6a4af1a.chunk.js"
+  },
+  {
+    "revision": "169e0e2dacae839997b5",
+    "url": "/static/js/main.36b8dea0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b07739c5a589f827776e75e5d05589a9",
+    "url": "/static/media/MemphisCrawfisCo.b07739c5.jpg"
+  },
+  {
+    "revision": "0e144583c37ab7b13b9e4ebd1000276c",
+    "url": "/static/media/blitz.0e144583.jpg"
+  },
+  {
+    "revision": "f28134afd99189a722fae5eec5057da5",
+    "url": "/static/media/codepen.f28134af.png"
+  },
+  {
+    "revision": "5c3ef0fbab365f033062c1b7583452de",
+    "url": "/static/media/github.5c3ef0fb.png"
+  },
+  {
+    "revision": "f9aacf68fabaeffed2c01ebeff38b9f9",
+    "url": "/static/media/many.f9aacf68.jpg"
+  },
+  {
+    "revision": "53107a934b785f0ca59e6d4fbee432da",
+    "url": "/static/media/myImage.53107a93.jpg"
+  }
+]);
